30d823a486cbbe4d5bb3cd17c3bc6916875fc50c
Aditya G <117929919+meri-git@users.noreply.github.com>
2023-07-26 12:57:49 -0400

    Update xml_to_delimited.py

--- TechnicalDocs/Python/xml_to_delimited.py
+++ TechnicalDocs/Python/xml_to_delimited.py
@@ -1,6 +1,13 @@
 import xml.etree.ElementTree as ET
 import csv
 
+def flatten_xml_child_nodes(element):
+    # Flatten the XML structure of a given element and its children into a single string
+    flattened_data = []
+    for child in element:
+        flattened_data.append(ET.tostring(child, encoding='unicode'))
+    return '\n'.join(flattened_data)
+
 def xml_to_delimited(input_file, output_file, delimiter='\t'):
     # Parse the XML file
     tree = ET.parse(input_file)
@@ -10,9 +17,9 @@ def xml_to_delimited(input_file, output_file, delimiter='\t'):
     with open(output_file, 'w', newline='') as delimited_file:
         csvwriter = csv.writer(delimited_file, delimiter=delimiter)
 
-        # Write each child element as a row in the delimited file
+        # Write each child node as a separate row in the delimited file
         for child in root:
-            row_data = [ET.tostring(child, encoding='unicode')]
+            row_data = [flatten_xml_child_nodes(child)]
             csvwriter.writerow(row_data)
 
 if __name__ == "__main__":
