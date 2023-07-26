f0902c7ff4a33e31d9a1939de315bd3b105612dc
Aditya G <117929919+meri-git@users.noreply.github.com>
2023-07-26 11:55:35 -0400

    Update xml_to_delimited.py

--- TechnicalDocs/Python/xml_to_delimited.py
+++ TechnicalDocs/Python/xml_to_delimited.py
@@ -1,4 +1,5 @@
 import xml.etree.ElementTree as ET
+import csv
 
 def xml_to_delimited(input_file, output_file, delimiter='\t'):
     # Parse the XML file
@@ -6,15 +7,13 @@ def xml_to_delimited(input_file, output_file, delimiter='\t'):
     root = tree.getroot()
 
     # Create a delimited file and write data
-    with open(output_file, 'w') as delimited_file:
-        # Write the header row based on the first child element
-        header = [child.tag for child in root[0]]
-        delimited_file.write(delimiter.join(header) + '\n')
+    with open(output_file, 'w', newline='') as delimited_file:
+        csvwriter = csv.writer(delimited_file, delimiter=delimiter)
 
         # Write each child element as a row in the delimited file
         for child in root:
-            row_data = [child.find(tag).text if child.find(tag) is not None else "" for tag in header]
-            delimited_file.write(delimiter.join(row_data) + '\n')
+            row_data = [ET.tostring(child, encoding='unicode')]
+            csvwriter.writerow(row_data)
 
 if __name__ == "__main__":
     input_xml_file = "input.xml"   # Replace with the path to your input XML file
