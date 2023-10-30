5c785c91059054bf1ffb86fdf9257b48ac9c6893
Aditya G <117929919+meri-git@users.noreply.github.com>
2023-10-30 15:14:53 -0400

    Update Oracle rate of data growth.sql

--- SQL/Oracle rate of data growth.sql
+++ SQL/Oracle rate of data growth.sql
@@ -27,3 +27,21 @@ JOIN
    AND TO_DATE(snap_time, 'DD-MON-YYYY HH24:MI:SS') <= TO_DATE('EndDateTime', 'DD-MON-YYYY HH24:MI:SS')) start_snap
 ON 1 = 1
 ORDER BY growth_rate_mb_per_day DESC;
+
+-- Alternative way
+WITH historical_data AS (
+    SELECT
+        backup_date,
+        SUM(bytes) / 1024 / 1024 AS data_size_mb
+    FROM v$backup_datafile
+    GROUP BY backup_date
+)
+SELECT
+    TO_CHAR(current_data.backup_date, 'YYYY-MM') AS month,
+    current_data.data_size_mb AS current_size_mb,
+    previous_data.data_size_mb AS previous_size_mb,
+    (current_data.data_size_mb - previous_data.data_size_mb) AS data_growth_mb,
+    ((current_data.data_size_mb - previous_data.data_size_mb) / previous_data.data_size_mb) * 100 AS growth_rate_percent
+FROM historical_data current_data
+LEFT JOIN historical_data previous_data
+    ON current_data.backup_date = previous_data.backup_date + 1;
